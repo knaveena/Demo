68e3868389f2a2f41675bf1caa7d98a83093aaf7
Naveena <nkadem1@gmail.com>
2018-04-17 23:27:27 -0500

    Mavenised the project.

--- src/com/java/practice/File_IO.java
+++ src/com/java/practice/File_IO.java
@@ -2,38 +2,37 @@ package com.java.practice;
 
 import java.io.*;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
-
+//import java.nio.charset.Charset;
+//import java.nio.charset.StandardCharsets;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.Scanner;
 
 public class File_IO {
-	
+
 	public static void main(String args[]) throws IOException {
-		
+
 		FileInputStream in = null;
 		FileOutputStream out = null;
-		
+
 		String inFile = "/Users/naveenakadem/Desktop/input.txt";
 		String outFile = "/Users/naveenakadem/Desktop/output.txt";
-		
+
 		try {
 			in = new FileInputStream(inFile);
 			out = new FileOutputStream(outFile);
-			
+
 			int c;
-			while((c = in.read()) != -1) {
+			while ((c = in.read()) != -1) {
 				out.write(c);
 			}
 		} finally {
-			if(in != null)
+			if (in != null)
 				in.close();
-			if(out != null)
+			if (out != null)
 				out.close();
 		}
-	
+
 	}
 
 }
